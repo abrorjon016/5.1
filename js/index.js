441da2747d17e50361dcb9fae4b42fe83b2263c3
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-11-23 01:59:31 +0500

    qoshib qoydim

---
+++ js/index.js
@@ -0,0 +1,196 @@
+// ### **1.--------------------------------
+// 1-masala
+// (function () {
+//   let num = [12, 3, 4, 5, 6, 78, 9];
+//   let sum = 0;
+//   for (let i = 0; i < num.length; i++) {
+//     sum += num[i];
+//   }
+//   console.log(sum);
+// })();
+
+// 2-masala
+// (function (a, b) {
+//   let res = a * b;
+//   console.log(res);
+// })();
+
+// ### **2.--------------------------------
+// 1 - masala
+// const shiron = ["bugatti"];
+// const tourbillion = ["bugatata"];
+// const res = shiron.concat(tourbillion);
+// console.log(res);
+
+// 2-masala
+// let bugatti = [1, 2, 4, 4, 5];
+// let res = [];
+// for (let i = 0; i < bugatti.length; i++) {
+//   res.push(bugatti[i] * 2);
+// }
+// return;
+
+// 3-masala
+// tushunmadim
+
+// ### **3.--------------------------------
+
+// 1 - masala
+// function bugatti(shiron, mistral) {
+//   let res = [...shiron ...mistral];
+//   return res
+// }
+
+// 2-masala
+// function bugatti(shiron) {
+//   let sum = 0;
+//   for (let i = 0; i < shiron.length; i++) {
+//     sum += shiron[i];
+//   }
+//   let ortaSon = sum / shiron.length;
+//   return ortaSon;
+// }
+
+// ### **4.--------------------------------
+
+// 1-masala
+// function bugatti(shiron, mistral) {
+//   let res = [];
+//   for (let i = 0; i < shiron.length; i++) {
+//     res.push(mistral(shiron[i]));
+//   }
+//   return res;
+// }
+
+// 2 - masala
+// function bugatti(shiron) {
+//   let res = [];
+//   for (let i = 0; i < shiron.length; i++) {
+//     if (shiron[i] % 2 !== 0) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   return res;
+// }
+
+// ### **5.--------------------------------
+
+// 1-masala
+// !
+
+// 2-masala
+// !
+
+// 3-masala
+// !
+
+// ### **6  .--------------------------------
+
+// 1-masala
+// function bugatti(shiron) {
+//   return Object.keys(shiron);
+// }
+
+// 2-masala
+// function bugatti(shiron) {
+//   return Object.values(shiron);
+// }
+
+// 3-masala
+// function bugatti(shiron) {
+//   return Object.entires(shiron);
+// }
+
+// ### **7  .--------------------------------
+
+// 1-masala
+// function bugatti(shiron) {
+//   let res = JSON.stringify(shiron);
+//   let res2 = JSON.parse(res);
+//   return res2;
+// }
+
+// 2-masala
+// function bugatti(shiron) {
+//   let res = JSON.stringify(shiron);
+//   let res2 = JSON.parse(res);
+//   return res2;
+// }
+
+// ### **8  .--------------------------------
+
+// 1-masala
+// function bugatti(shiron) {
+//   return shiron.split(" ").join("");
+// }
+
+// 2-masala
+// function bugatti(shiron) {
+//   return shiron.toUpperCase();
+// }
+
+// 3-masala
+// let bugatti = "shiron";
+// console.log(capitalizeFirstLetter(bugatti));
+// funksiya qilalmadim
+
+// 4-masala
+// function bugatti(shiron) {
+//   return shiron.trimEnd();
+// }
+
+// 5-masala
+// function bugatti(shiron) {
+//   return shiron.split(" ");
+// }
+
+// 6-masala
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+
+// 7-masala
+// function bugatti(text, word) {
+//   let counter = 0;
+
+//   for (const i of text.split(" ")) {
+//     if (i === word) counter++;
+//   }
+
+//   return counter;
+// }
+
+// 8-masala
+// ?
+
+// 9-Misol
+// function bugatti(shiron, index) {
+//   return shiron.slice(index);
+// }
+
+// 10-Misol
+// function bugatti(shiron, index) {
+//   return shiron.slice(0, index);
+//}
+
+// 11-masala
+// function bugatti(shiron, mistral) {
+//   return bugatti.replace(shiron, tourbillion);
+// }
+
+// 12-masala
+// function bugatti(shiron) {
+//   return shiron.toLowerCase();
+// }
+
+// 13-masala
+// ?
+
+// 15-masala
+// function bugatti(shiron, mistral) {
+//   if (shiron.includes(mistral)) {
+//     let res = mistral.split("").reverse().join("");
+//     return shiron.replace(mistral, reversedWord);
+//   }
+//   return shiron;
+// }
